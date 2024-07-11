5380d2697fde34563ee2c2b9aef316693ae95164
or77iu <2945515386@qq.com>
2024-07-11 17:56:58 +0800

    feat:向路由组件传递params参数

---
+++ src/pages/Home/Message/Details/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+const data = [
+  {
+    id: "01",
+    content: "111",
+  },
+  {
+    id: "02",
+    content: "222",
+  },
+  {
+    id: "03",
+    content: "333",
+  },
+  {
+    id: "04",
+    content: "444",
+  },
+];
+export default class Detail extends Component {
+  render() {
+    // 接受params参数
+    const { id, title } = this.props.match.params;
+    const findById = data.find((item) => {
+      return item.id == id;
+    });
+    return (
+      <div>
+        <ul>
+          <li>id:{id}</li>
+          <li>title:{title}</li>
+          <li>conten:{findById.content}</li>
+        </ul>
+      </div>
+    );
+  }
+}
