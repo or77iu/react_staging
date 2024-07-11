88e0dba8d35c58229a14f237917300cfce968369
or77iu <2945515386@qq.com>
2024-07-11 16:37:05 +0800

    feat:使用redirect重定向

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch,Redirect } from "react-router-dom";
 import Home from "./pages/Home"; //路由组件
 import About from "./pages/About"; //路由组件
 import Header from "./components/Header"; //一般组件
@@ -19,18 +19,11 @@ export default class App extends Component {
         <div className="row">
           <div className="col-xs-2 col-xs-offset-2">
             <div className="list-group">
-              {/* 原生html中靠a标签跳转 */}
-              {/* <a className="list-group-item" href="./about.html">
-                About
-              </a>
-              <a className="list-group-item active" href="./home.html">
-                Home
-              </a> */}
               {/* 在react中靠路由链接实现切换组件 --编写路由链接*/}
               <MyNavLink to="/about" title="About">
                 About
               </MyNavLink>
-              <MyNavLink to="/home/a/b" title="Home">
+              <MyNavLink to="/home" title="Home">
                 Home
               </MyNavLink>
             </div>
@@ -39,16 +32,10 @@ export default class App extends Component {
             <div className="panel">
               <div className="panel-body">
                 {/* 注册路由 */}
-                {/* 使用Switch后只会展示第一个/home路径匹配的组件 
-                    Switch可以提高路由匹配效率(单一匹配)
-                */}
-                {/*
-                    默认为模糊匹配，设置exact=true开启精准匹配
-                    严格匹配不要随便开启，需要再开，有时候开启会导致无法匹配二级路由
-                */}
                 <Switch>
-                  <Route exact={true} path="/about" component={About} />
-                  <Route exact path="/home" component={Home} />
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                  <Redirect to="/about"/>
                 </Switch>
               </div>
             </div>
