72391278c5da6cc0f75a295daaccd3f40bd34768
or77iu <2945515386@qq.com>
2024-07-11 17:22:52 +0800

    feat:二级路由的使用

--- 12_src_二级路由的使用/pages/Home/index.jsx
+++ 12_src_二级路由的使用/pages/Home/index.jsx
@@ -18,6 +18,14 @@ export default class index extends Component {
             </li>
           </ul>
           {/* 注册路由 */}
+          {/* 
+          1.一般写在所有路由注册的最下方，当所有路由都无法匹配时，跳转到Redirect指定的路由
+			    	2.具体编码：
+						`<Switch>`
+							`<Route path="/about" component={About}/>`
+							`<Route path="/home" component={Home}/>`
+							`<Redirect to="/about"/>`
+						`</Switch>` */}
           <Switch>
             <Route path="/home/news" component={News} />
             <Route path="/home/message" component={Message} />
