1db05043e29c6e0162308d3bcd6724a3d82bd925
or77iu <2945515386@qq.com>
2024-07-11 20:40:10 +0800

    feat:向路由传递search参数

---
+++ 14_src_向路由组件传递search参数/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Route, Switch,Redirect } from "react-router-dom";
+import Home from "./pages/Home"; //路由组件
+import About from "./pages/About"; //路由组件
+import Header from "./components/Header"; //一般组件
+import MyNavLink from "./components/MyNavLink";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              {/* <h2>React Router Demo</h2> */}
+              <Header />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 在react中靠路由链接实现切换组件 --编写路由链接*/}
+              <MyNavLink to="/about" title="About">
+                About
+              </MyNavLink>
+              <MyNavLink to="/home" title="Home">
+                Home
+              </MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                  <Redirect to="/about"/>
+                </Switch>
+              </div>
+            </div>
+          </div> 
+        </div>
+      </div>
+    );
+  }
+}
